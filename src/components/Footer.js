931992da4b6c9a759e9fa609af21081f24ef9c24
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-08-26 20:57:53 -0400

    Material UI is back in the mix, got a footer partially working in that it's on the screen, but not in the right spot.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import {Container} from '@material-ui/core';
-import Row from '@material-ui/core/';
-import Col from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
-function Footer()  {
+const useStyles = makeStyles({
+  root: {
+    width: 325,
+  },
+});
 
-    return(
-        <footer className="mt-5">
-            <Container fluid={true}>
-                {/* <Row className="border-top justify-content-between p-3">
-                    <Col className="p-0" md={3} sm= {12}>
-                        Grubble
-                    </Col>
-                    <Col className="p-0 d-flex justify-content-end" md={3}>
-                        This site was made by the Lost Souls 
-                    </Col>
-                </Row> */}
-            </Container>
-        </footer>
-    )
+export default function SimpleBottomNavigation() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+    </BottomNavigation>
+  );
 }
-
-export default Footer;

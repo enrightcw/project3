275e532f0e1901117b9d3d544bc0627de28b02a6
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-09-01 19:13:32 -0400

    Moved display code to its own page to clean up the App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import {BrowserRouter as Router, Route } from 'react-router-dom'; 
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
+// import {BrowserRouter as Router, Route } from 'react-router-dom'; 
+// import Container from '@material-ui/core/Container';
+// import Box from '@material-ui/core/Box';
 import './App.css';
-<<<<<<< HEAD
 
-import axios from 'axios';
-=======
->>>>>>> master
+// import axios from 'axios';
 
 import Footer from './components/Footer';
-import Jumbotron from './components/Jumbotron';
-import Home from './pages/Home';
-import Quiz from './pages/Quiz';
-import Search from './pages/Search';
-import Results from './pages/Results';
-import Friends from './pages/Friends';
+import Display from './pages/Display';
+
 
 class App extends React.Component {
 
@@ -46,35 +39,14 @@ constructor(props) {
       }
     }
   }
-
+ 
 
   render(){
     return (
-      <Router>
-        <Container className="p-0 container" fluid={true}>
-        <Box component="span" m={1} container
-          direction="column"
-          justify="flex-end"
-          alignItems="center">
-          <Jumbotron>
-            <h1 className="title">Grubble</h1>
-          </Jumbotron>
-          
-          <Route path="/" exact render={()=> <Home title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
-          <Route path="/search" exact render={()=> <Search title={this.state.search.title} />} />
-          <Route path="/results" exact render={()=> <Results title={this.state.results.title}/>} />
-          <Route path="/friends" exact render={()=> <Friends title={this.state.friends.title}/>} />
-          <Route path="/quiz" exact render={()=> <Quiz title={this.state.quiz.title}/>} />
-          </Box>
-          <Footer />
-          {/* <Link className="nav-link" to ="/">Home </Link>
-          <Link className="nav-link" to ="/food">Food</Link>
-          <Link className="nav-link" to ="/entertainment">Entertainment</Link>
-          <Link className="nav-link" to ="/friends">Friends</Link> */}
-
-
-        </Container>
-      </Router>
+      <div>
+        <Display />
+        <Footer />
+      </div>
     );
   }
  
